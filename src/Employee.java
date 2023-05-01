faae605845f958db23b7129152d2b29a36242ffd
Truc Pham <trucphamhmc@gmail.com>
2023-05-01 16:15:31 +0700

    Java Employee

---
+++ src/Employee.java
@@ -0,0 +1,58 @@
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    // Constructor
+    public Employee(){}
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+
+    //Setter and getter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    //Get fullName
+    public String getFullName() {
+        return (firstName + " " + lastName);
+    }
+
+
+
+
+}
